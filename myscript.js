46ed4282257f45a1f85ec55ebfcbe3f9a847ccd5
RiccardoImperato <riccardo.imperato@libero.it>
2023-11-06 20:47:34 +0100

    fix: correzione esercizio

--- myscript.js
+++ myscript.js
@@ -4,21 +4,18 @@
 
 const elementNome = document.getElementById('nome');
 const elementCognome = document.getElementById('cognome');
-const elementPassword = document.getElementById('password');
 const elementColore = document.getElementById('colore');
 const elementGenerata = document.getElementById('generata');
 
-elementGenerata.innerHTML = 'Nuova password: '
+const elementNumero = Math.round(Math.random() * 100)
 
 elementNome.innerHTML = prompt('Inserisci il tuo nome')
 elementCognome.innerHTML = prompt('Inserisci il tuo cognome')
-elementPassword.innerHTML = prompt('Inserisci La tua password')
 elementColore.innerHTML = prompt('Inserisci il tuo colore preferito')
-elementGenerata.innerHTML += elementNome.innerHTML + elementCognome.innerHTML + elementColore.innerHTML + (21)
+elementGenerata.innerHTML = elementNome.innerHTML + elementCognome.innerHTML + elementColore.innerHTML + elementNumero
 
 console.log(elementNome.innerHTML);
 console.log(elementCognome.innerHTML);
-console.log(elementPassword.innerHTML);
 console.log(elementColore.innerHTML);
 console.log(elementGenerata.innerHTML);
 
